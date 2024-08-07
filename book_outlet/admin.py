49e16b784d94a39612bda9b6024f2c08d0572d38
Aasis <aasispaudel@26gmail.com>
2024-08-07 15:52:23 +0545

    first commit

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class Bookadmin(admin.ModelAdmin):
+  list_filter = ("author","rating",)
+  list_display = ("author","title","rating")
+
+
+admin.site.register(Book,Bookadmin)
